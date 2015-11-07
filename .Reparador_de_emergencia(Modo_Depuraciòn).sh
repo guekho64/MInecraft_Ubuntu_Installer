811bb1910ee1c5b2c3ac08a94535d2a2bc5ddaa8
guekho64 <guekho64@users.noreply.github.com>
2015-11-06 21:51:32 -0600

    An script made to fix some common errors caused by missing dependencies. Features colorless text and full log

---
+++ .Reparador_de_emergencia(Modo_Depuraciòn).sh
@@ -0,0 +1,49 @@
+#Copyright guekho64 © 2015
+
+#Zona Especial
+
+#Define variables de colores:
+subrayado=$(tput sgr 0 1)    
+negritas=$(tput bold)   
+null=$(tput sgr0)      
+
+##Rescatador
+echo "${negritas}Recuerda: Cualquier cosa que ande mal, o algùn comentario que deseara mandar, 
+favor de envìar a: guekho64@gmail.com${null}"
+sleep 6.4
+clear
+echo "${negritas}Este script instalarà las posibles dependencias faltantes para el arranque del 
+Instalador. Necesitas internet para poder utilizar tanto el Instalador como este script${null}"
+sleep 6.4
+clear
+echo "${negritas}Probando conexiòn a Internet..."
+sleep 3.2
+( ping -c 4 www.google.com.mx ) 
+if [ $? -ne 0 ]; then
+clear
+	echo "${negritas}Al parecer usted no esta conectado a Internet${null}";
+	sleep 3.2
+	clear
+	echo "${negritas}Conèctese a Internet y vuelva a Intentarlo...${null}"
+		sleep 3.2
+		exit
+else
+clear
+echo "${negritas}OK${null}"
+sleep 2
+clear
+echo "${negritas}Introduzca su contraseña para continuar${null}"
+		clear
+		echo "${negritas}Trabajando...${null}"
+( sudo apt update;sudo apt install xterm gnome-terminal -y ) 
+if [ $? -ne 0 ]; then
+	echo "${negritas}Algo salió mal, cerrando el programa...${null}";
+	sleep 3.2
+	exit
+else
+clear
+echo "${negritas}Listo, puedes continuar con el Instalador${null}"
+sleep 3.2
+exit
+fi;
+fi;
