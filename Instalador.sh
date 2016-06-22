6cee48ae4348203a6bff672b1d477a7abeffa749
guekho64 <guekho64@users.noreply.github.com>
2016-06-22 17:30:37 -0500

    Update Instalador.sh

--- Instalador.sh
+++ Instalador.sh
@@ -2120,6 +2120,12 @@ Terminal=false'
     
 Inicio () {
 
+#Un Bug MUY Peculiar si es que ya tenías la carpeta .guekho64 antes...
+
+autosudo chmod 777 "$HOME/.guekho64"
+
+#ARREGLADO
+
 tput reset
 
 if [ "$Passwd" = "$Nada" ]; then
